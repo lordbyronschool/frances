a9f1c31d95b52cf508edea682c8026e49c676f7c
lordbyronschool <jrodriguez@byron.edu.pe>
2019-10-27 22:16:13 -0500

    redireccion

--- script/menu.js
+++ script/menu.js
@@ -1,43 +1,43 @@
 $(document).ready(function(){
 
 	$("#Danser").click(function(){
-     var url = "Danser.html"; 
+     var url = "pages/Danser.html"; 
      $(location).attr('href',url);
      });
 	$("#Chanter").click(function(){
-     var url = "Chanter.html"; 
+     var url = "pages/Chanter.html"; 
      $(location).attr('href',url);
      });
 	$("#Parler").click(function(){
-     var url = "Parler.html"; 
+     var url = "pages/Parler.html"; 
      $(location).attr('href',url);
      });
 	$("#Ecouter").click(function(){
-     var url = "Ecouter.html"; 
+     var url = "pages/Ecouter.html"; 
      $(location).attr('href',url);
      });
 	$("#Manger").click(function(){
-     var url = "Manger.html"; 
+     var url = "pages/Manger.html"; 
      $(location).attr('href',url);
      });
 	$("#Jouer").click(function(){
-     var url = "Jouer.html"; 
+     var url = "pages/Jouer.html"; 
      $(location).attr('href',url);
      });
 	$("#Regarder").click(function(){
-     var url = "Regarder.html"; 
+     var url = "pages/Regarder.html"; 
      $(location).attr('href',url);
      });
 	$("#Etudier").click(function(){
-     var url = "Etudier.html"; 
+     var url = "pages/Etudier.html"; 
      $(location).attr('href',url);
      });
 	$("#Aimer").click(function(){
-     var url = "Aimer.html"; 
+     var url = "pages/Aimer.html"; 
      $(location).attr('href',url);
      });
 	$("#Habiter").click(function(){
-     var url = "Habiter.html"; 
+     var url = "pages/Habiter.html"; 
      $(location).attr('href',url);
      }); 
 });
