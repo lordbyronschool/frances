5bb77ce7ce55223a9fb5eada482d91bc02c60a33
Jhonson Rodriguez <sukerjc@gmail.com>
2019-10-29 15:23:26 -0500

    transport

--- script/menu.js
+++ script/menu.js
@@ -1,43 +1,43 @@
 $(document).ready(function(){
 
 	$("#Danser").click(function(){
-     var url = "../pages/Danser.html"; 
+     var url = "../transport/pages/Danser.html"; 
      $(location).attr('href',url);
      });
 	$("#Chanter").click(function(){
-     var url = "../pages/Chanter.html"; 
+     var url = "../transport/pages/Chanter.html"; 
      $(location).attr('href',url);
      });
 	$("#Parler").click(function(){
-     var url = "../pages/Parler.html"; 
+     var url = "../transport/pages/Parler.html"; 
      $(location).attr('href',url);
      });
 	$("#Ecouter").click(function(){
-     var url = "../pages/Ecouter.html"; 
+     var url = "../transport/pages/Ecouter.html"; 
      $(location).attr('href',url);
      });
 	$("#Manger").click(function(){
-     var url = "../pages/Manger.html"; 
+     var url = "../transport/pages/Manger.html"; 
      $(location).attr('href',url);
      });
 	$("#Jouer").click(function(){
-     var url = "../pages/Jouer.html"; 
+     var url = "../transport/pages/Jouer.html"; 
      $(location).attr('href',url);
      });
 	$("#Regarder").click(function(){
-     var url = "../pages/Regarder.html"; 
+     var url = "../transport/pages/Regarder.html"; 
      $(location).attr('href',url);
      });
 	$("#Etudier").click(function(){
-     var url = "../pages/Etudier.html"; 
+     var url = "../transport/pages/Etudier.html"; 
      $(location).attr('href',url);
      });
 	$("#Aimer").click(function(){
-     var url = "../pages/Aimer.html"; 
+     var url = "../transport/pages/Aimer.html"; 
      $(location).attr('href',url);
      });
 	$("#Habiter").click(function(){
-     var url = "../pages/Habiter.html"; 
+     var url = "../transport/pages/Habiter.html"; 
      $(location).attr('href',url);
      }); 
 });
