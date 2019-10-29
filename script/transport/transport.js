099cdf932db9818fabe049a2f45e74448cb6e301
Jhonson Rodriguez <sukerjc@gmail.com>
2019-10-29 15:18:29 -0500

    final

---
+++ script/transport/transport.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+	$("#train").click(function(){
+     var url = "../transport/pages/train.html"; 
+     $(location).attr('href',url);
+     });
+	$("#voiture").click(function(){
+     var url = "../transport/pages/voiture.html"; 
+     $(location).attr('href',url);
+     });
+	$("#moto").click(function(){
+     var url = "../transport/pages/moto.html"; 
+     $(location).attr('href',url);
+     });
+	$("#velo").click(function(){
+     var url = "../transport/pages/velo.html"; 
+     $(location).attr('href',url);
+     });
+	$("#bus").click(function(){
+     var url = "../transport/pages/bus.html"; 
+     $(location).attr('href',url);
+     });
+	$("#avion").click(function(){
+     var url = "../transport/pages/avion.html"; 
+     $(location).attr('href',url);
+     });
+	$("#bateau").click(function(){
+     var url = "../transport/pages/bateau.html"; 
+     $(location).attr('href',url);
+     });
+	
+	$("#volver").click(function(){
+     var url = "../../transport/transport.html"; 
+     $(location).attr('href',url);
+     });
+
+});
+
+
