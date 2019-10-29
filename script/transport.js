45ad17f8f6d3160531c18a9fb5f8931fbd7d55a2
Jhonson Rodriguez <sukerjc@gmail.com>
2019-10-29 15:36:06 -0500

    ley

--- script/transport.js
+++ script/transport.js
@@ -1,36 +1,36 @@
 $(document).ready(function(){
 
 	$("#train").click(function(){
-     var url = "../../transport/pages/train.html"; 
+     var url = "transport/pages/train.html"; 
      $(location).attr('href',url);
      });
 	$("#voiture").click(function(){
-     var url = "../../transport/pages/voiture.html"; 
+     var url = "transport/pages/voiture.html"; 
      $(location).attr('href',url);
      });
 	$("#moto").click(function(){
-     var url = "../../transport/pages/moto.html"; 
+     var url = "transport/pages/moto.html"; 
      $(location).attr('href',url);
      });
 	$("#velo").click(function(){
-     var url = "../../transport/pages/velo.html"; 
+     var url = "transport/pages/velo.html"; 
      $(location).attr('href',url);
      });
 	$("#bus").click(function(){
-     var url = "../../transport/pages/bus.html"; 
+     var url = "transport/pages/bus.html"; 
      $(location).attr('href',url);
      });
 	$("#avion").click(function(){
-     var url = "../../transport/pages/avion.html"; 
+     var url = "transport/pages/avion.html"; 
      $(location).attr('href',url);
      });
 	$("#bateau").click(function(){
-     var url = "../../transport/pages/bateau.html"; 
+     var url = "transport/pages/bateau.html"; 
      $(location).attr('href',url);
      });
 	
 	$("#volver").click(function(){
-     var url = "../../../transport/transport.html"; 
+     var url = "transport/transport.html"; 
      $(location).attr('href',url);
      });
 
